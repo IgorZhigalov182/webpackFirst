f2356a9e0112c7a71e909b8ba23641fa706b76d5
Igor Zhigalov <Igor>
2022-11-30 21:32:34 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+import './index.css';
+import jsimg from './assets/img.jpg'
 console.log('hello world!');
+
+const imgFirst = document.createElement('img');
+imgFirst.src = jsimg;
+
+document.body.append(jsimg);
